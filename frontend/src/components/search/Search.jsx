7935cbc4dd1e37453ff4ddd78efe95cf0308b293
chamanos <chamano.sr@gmail.com>
2023-08-31 12:40:28 +0200

    removing notification icon nd user icons

--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiOutlineSearch, AiOutlineBell } from 'react-icons/ai'; // Importing icons
+import { AiOutlineSearch } from 'react-icons/ai';
 import logo from "../../assets/images/dev.png";
 import './search.css';
 
@@ -13,19 +13,13 @@ export const Search = () => {
         <div className="search-bar">
           <input type="text" id="searchInput" placeholder="Search..." />
           <button id="searchButton">
-            <AiOutlineSearch className="search-icon" /> {/* Using the search icon */}
+            <AiOutlineSearch className="search-icon" /> 
           </button>
         </div>
       </div>
 
       <div className="actions">
         <button id="createPostButton">Create Post</button>
-        <div className="notifications">
-          <AiOutlineBell className="notification-icon" /> {/* Using the notification icon */}
-        </div>
-        <div className="user-image">
-          <img src="user-img.jpg" alt="" />
-        </div>
       </div>
     </div>
   );
