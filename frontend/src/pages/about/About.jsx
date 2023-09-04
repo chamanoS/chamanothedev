70f72e3f280ea6c394edf04c78494e22074c75f3
chamanos <chamano.sr@gmail.com>
2023-09-04 12:13:41 +0200

    setting the about me structure

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import './about.css'
+import about from '../../assets/images/Sparkle.png'
 
 export const About = () => {
   return (
     <>
-    <section>
-      <div className='about-me'>
-       <h1>About Me</h1>
+      <section className='the-body'>
+        <div className='about-me'>
+          <h1>About Me</h1>
         </div>
-    </section>
+        <div className='hero-section'>
+          <div className='about-img'>
+            <img src={about} alt='' />
+          </div>
+          <div className='about-content'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Magni, quibusdam sequi vitae ducimus distinctio aspernatur
+              odio et expedita veritatis necessitatibus. Ea quam perspiciatis
+              ad deserunt ducimus libero voluptatem earum temporibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Magni, quibusdam sequi vitae ducimus distinctio aspernatur
+              odio et expedita veritatis necessitatibus. Ea quam perspiciatis
+              ad deserunt ducimus libero voluptatem earum temporibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Magni, quibusdam sequi vitae ducimus distinctio aspernatur
+              odio et expedita veritatis necessitatibus. Ea quam perspiciatis
+              ad deserunt ducimus libero voluptatem earum temporibus.</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Magni, quibusdam sequi vitae ducimus distinctio aspernatur
+              odio et expedita veritatis necessitatibus. Ea quam perspiciatis
+              ad deserunt ducimus libero voluptatem earum temporibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Magni, quibusdam sequi vitae ducimus distinctio aspernatur
+              odio et expedita veritatis necessitatibus. Ea quam perspiciatis
+              ad deserunt ducimus libero voluptatem earum temporibus.</p>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
