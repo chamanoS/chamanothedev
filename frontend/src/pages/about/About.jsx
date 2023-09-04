f517cc0e8329ad61e4b437f82fee82f8ee65e414
chamanos <chamano.sr@gmail.com>
2023-09-04 12:52:47 +0200

    setting up the experience section

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -36,6 +36,33 @@ export const About = () => {
               ad deserunt ducimus libero voluptatem earum temporibus.</p>
           </div>
         </div>
+
+        <div className='experience'>
+        <div className='about-me'>
+          <h1>Work Experience</h1>
+        </div>
+        <div className='titles'>
+          <h2>Company Name</h2>
+          <hr/>
+          <h2>2019 - 2020</h2>
+        </div>
+        <div className='job'>
+          <h3>Job Title</h3>
+        </div>
+        <ul className='lists'>
+          <li>* Lorem ipsum dolor sit amet consectetur </li>
+          <li>* Lorem ipsum dolor sit amet consectetur </li>
+          <li>* Lorem ipsum dolor sit amet consectetur </li>
+          <li>* Lorem ipsum dolor sit amet consectetur </li>
+          <li>* Lorem ipsum dolor sit amet consectetur </li>
+        </ul>
+        </div>
+
+        <div className='devider'>
+          <div class="circle"></div>
+          <div class="vertical-line"></div>
+          <div class="circle"></div>
+        </div>
       </section>
     </>
   )
