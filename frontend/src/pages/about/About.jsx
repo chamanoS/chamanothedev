497d81b26174a3fb58b8a589fff81548df24e465
chamanos <chamano.sr@gmail.com>
2023-09-06 15:26:39 +0200

    markeing banner

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -26,7 +26,7 @@ export const About = () => {
               Magni, quibusdam sequi vitae ducimus distinctio aspernatur
               odio et expedita veritatis necessitatibus. Ea quam perspiciatis
               ad deserunt ducimus libero voluptatem earum temporibus.</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Magni, quibusdam sequi vitae ducimus distinctio aspernatur
               odio et expedita veritatis necessitatibus. Ea quam perspiciatis
               ad deserunt ducimus libero voluptatem earum temporibus.</p>
@@ -37,32 +37,65 @@ export const About = () => {
           </div>
         </div>
 
-        <div className='experience'>
-        <div className='about-me'>
-          <h1>Work Experience</h1>
-        </div>
-        <div className='titles'>
-          <h2>Company Name</h2>
-          <hr/>
-          <h2>2019 - 2020</h2>
-        </div>
-        <div className='job'>
-          <h3>Job Title</h3>
-        </div>
-        <ul className='lists'>
-          <li>* Lorem ipsum dolor sit amet consectetur </li>
-          <li>* Lorem ipsum dolor sit amet consectetur </li>
-          <li>* Lorem ipsum dolor sit amet consectetur </li>
-          <li>* Lorem ipsum dolor sit amet consectetur </li>
-          <li>* Lorem ipsum dolor sit amet consectetur </li>
-        </ul>
+        <div className='experiences'>
+          <div className='about-me'>
+            <h1>Work Experience</h1>
+          </div>
+
+          <div className='experience'>
+            <div className='titles'>
+              <h2>Company Name</h2>
+              <hr />
+              <h2>2019 - 2020</h2>
+            </div>
+            <div className='job'>
+              <h3>Job Title</h3>
+            </div>
+            <ul className='lists'>
+              <li>* Lorem ipsum dolor sit amet consectetur </li>
+              <li>* Lorem ipsum dolor sit amet consectetur </li>
+              <li>* Lorem ipsum dolor sit amet consectetur </li>
+              <li>* Lorem ipsum dolor sit amet consectetur </li>
+              <li>* Lorem ipsum dolor sit amet consectetur </li>
+            </ul>
+          </div>
+          <div className='devider'>
+            <div class="circle"></div>
+            <div class="vertical-line"></div>
+            <div class="circle"></div>
+          </div>
+
+          <div className='experience'>
+            <div className='titles'>
+              <h2>Company Name</h2>
+              <hr />
+              <h2>2021 - present</h2>
+            </div>
+            <div className='job'>
+              <h3>Job Title</h3>
+            </div>
+            <ul className='lists'>
+              <li>* Lorem ipsum dolor sit amet consectetur </li>
+              <li>* Lorem ipsum dolor sit amet consectetur </li>
+              <p>See more...</p>
+            </ul>
+          </div>
+
+          <div>
+            <div>
+              <h1>Lets start a project together</h1>
+              <p>Have a project in mind? </p>
+              <p>send me and email and lets take if from there:) </p>
+              <p>yahudah.sr@thedev.com </p><br/>
+              <p>Connect with on all social media</p>
+              <p>I would like to meet up with you and discuss all possible collaboration</p>
+            </div>
+          </div>
         </div>
 
-        <div className='devider'>
-          <div class="circle"></div>
-          <div class="vertical-line"></div>
-          <div class="circle"></div>
-        </div>
+
+
+
       </section>
     </>
   )
