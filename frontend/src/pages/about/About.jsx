88deb798136807c277d333dd281d42723bd65e83
chamanos <chamano.sr@gmail.com>
2023-09-11 15:58:58 +0200

    new design

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -5,6 +5,13 @@ export const About = () => {
   return (
     <>
       <section className='the-body'>
+        <div className='bold-lines'>
+          <div className='lines upper'></div>
+          <div className='portfolio'>
+            <h1>* PORTFOLIO *</h1>
+          </div>
+          <div className='lines lower'></div>
+        </div>
         <div className='about-me'>
           <h1>About Me</h1>
         </div>
